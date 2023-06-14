e2542053d7dd2d3adbe3bf0c090f5aa559f1a902
Lasseee04 <104563390+Lasseee04@users.noreply.github.com>
2023-06-14 13:16:37 +0200

    Add files via upload

---
+++ pages/auth/helper/login.php
@@ -0,0 +1,42 @@
+<?php
+$nutzername = $_POST['nutzername'];
+$passwort = $_POST['passwort'];
+
+$servername = "localhost"; 
+$username = "username"; 
+$password = "password"; 
+$dbname = "myDB"; 
+
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname); 
+// Check connection 
+if ($conn->connect_error) { 
+    die("Connection failed: " . $conn->connect_error); 
+} 
+
+//login
+
+$sql = "SELECT * from User WHERE nutzername = '".$nutzername."'"; 
+$result = $conn->query($sql); 
+
+
+if ($result->num_rows > 0) { 
+    //output data of each row 
+    $user = $result->fetch_assoc();
+            //nutzer existiert
+            if(password_verify($passwort, $user["passwort"])){
+                //passwort für diesen Nuter stimmt
+                //anmelden
+                session_start();
+                $_SESSION['nutzername'] = $nutzername;
+                header('LOCATION ../../index.html');
+            }else{
+                echo "Das Passwort ist Falsch";
+            }
+}else{
+    echo "Der Nutzername existiert nicht";
+}
+
+$conn->close(); 
+exit();
+?> 
--- pages/auth/helper/signup.php
+++ pages/auth/helper/signup.php
@@ -1,9 +1,62 @@
 <?php
-$name = $_POST['name'];
-$password = $_POST['password'];
+$nutzername = $_POST['nutzername'];
+$passwort = $_POST['passwort'];
 
-session_start();
-$_SESSION['name'] = $name;
+$servername = "localhost"; 
+$username = "username"; 
+$password = "password"; 
+$dbname = "myDB"; 
+
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname); 
+// Check connection 
+if ($conn->connect_error) { 
+    die("Connection failed: " . $conn->connect_error); 
+} 
+
+$sql = "SELECT * from User where username = $name"; 
+$result = $conn->query($sql); 
+
+//check if username not taken
+if($result-> num_rows <= 0){
+    //not taken
+
+    //create account
+    $sql = "INSERT INTO User (nutzername, passwort) VALUES ($nutzername, $passwort)"; 
+    if ($conn->query($sql) === TRUE) { 
+        echo "New record created successfully"; 
+    } else { 
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    //login
+    session_start();
+    $_SESSION['nutzername'] = $nutzername;
+    header('LOCATION ../../index.html');
+
+}else{
+    //username already exists
+    echo "Dieser Nutzername ist nicht verfügbar."
+}
+
+
+
+if ($result->num_rows > 0) { 
+    // output data of each row 
+    while($row = $result->fetch_assoc()) { 
+
+        --echo "id: ".$row["id"]." - Saison: ".$row["saison"]."<br>"; 
+    } 
+
+} else { 
+
+    echo "0 results"; 
+
+} 
+
+$conn->close(); 
+
 
 header('LOCATION ../../index.html');
 exit();
+?> 
+
